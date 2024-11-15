aa85c2d3e78cadb53210b1c7995863615bbd858b
VanezKun <fanes23.pratama@mhs.mdp.ac.id>
2024-11-15 16:14:57 +0700

    add-angular: routing

--- myAngular/src/app/app.component.spec.ts
+++ myAngular/src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myAngular' title`, () => {
+  it(`should have the 'my-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myAngular');
+    expect(app.title).toEqual('my-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, my-app',
+    );
   });
 });
