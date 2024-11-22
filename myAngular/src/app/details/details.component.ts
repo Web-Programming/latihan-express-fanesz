a07af570c5fb7cb7e029499c3415055fc9fd5d69
VanezKun <fanes23.pratama@mhs.mdp.ac.id>
2024-11-22 16:57:14 +0700

    add-angular: improvement to housing details

--- myAngular/src/app/details/details.component.ts
+++ myAngular/src/app/details/details.component.ts
@@ -1,44 +1,94 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <section *ngIf="housingDetails" class="details">
+    <article>
       <img
-        class="details-photo"
-        [src]="this.baseUrl + housingDetails.photo"
-        alt="Exterior photo of {{ housingDetails.name }}"
+        class="listing-photo"
+        [src]="this.baseUrl + housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
       />
-      <h2 class="details-heading">{{ housingDetails.name }}</h2>
-      <p class="details-location">
-        {{ housingDetails.city }}, {{ housingDetails.state }}
-      </p>
-      <p class="details-unit">{{ housingDetails.availableUnits }} Unit(s)</p>
-    </section>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+          <li>
+            Does this location have laundry: {{ housingLocation?.laundry }}
+          </li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input
+            type="text"
+            id="first-name"
+            formControlName="firstName"
+            placeholder="Input first name"
+          />
+          <label for="last-name">Last Name</label>
+          <input
+            type="text"
+            id="last-name"
+            formControlName="lastName"
+            placeholder="Input last name"
+          />
+
+          <label for="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            formControlName="email"
+            placeholder="Input email"
+          />
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css',
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | null = null;
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  housingLocationId = 0;
-  housingDetails: HousingLocation | null = null;
-
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    console.log('this.housingLocationId', this.housingLocationId);
     this.housingService
       .getHousingLocationById(this.housingLocationId)
       .then((data) => {
-        this.housingDetails = data;
+        this.housingLocation = data;
       });
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
